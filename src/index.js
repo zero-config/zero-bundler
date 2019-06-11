4c6854dee274debe0e0de059d70898e1e3e9b98b
Victor Wang <wang_shaojie@icloud.com>
2019-06-11 18:26:56 +0800

    initial commit -m

---
+++ src/index.js
@@ -0,0 +1,59 @@
+import { rollup } from 'rollup';
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import { join } from 'path';
+import { existsSync, readFileSync } from 'fs';
+
+const rootPath = process.cwd();
+
+const build = async (pkgJson) => {
+  if (!pkgJson) {
+    return;
+  }
+  const extensions = ['.mjs', '.js', '.jsx', '.ts', 'tsx'];
+  const { source, main, module } = pkgJson;
+  const inputOptions = {
+    input: join(rootPath, source),
+    plugins: [
+      typescript(),
+      babel({
+        exclude: 'node_modules/**',
+      }),
+      resolve({
+        extensions,
+      }),
+      commonjs({
+        extensions,
+      }),
+    ],
+  };
+  const outputOptions = [
+    {
+      format: 'cjs',
+      file: join(rootPath, main),
+    },
+  ];
+
+  if (module) {
+    outputOptions.push({
+      format: 'esm',
+      file: join(rootPath, module),
+    });
+  }
+
+  // create a bundle
+  const bundle = await rollup(inputOptions);
+
+  // generate code
+  await Promise.all(
+    outputOptions.map(option => bundle.write(option)),
+  );
+};
+
+
+const pkgPath = join(rootPath, 'package.json');
+const pkgJson = existsSync(pkgPath) ? JSON.parse(readFileSync(pkgPath, 'utf8')) : undefined;
+
+build(pkgJson).catch(console.log);
